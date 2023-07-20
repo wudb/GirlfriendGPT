68bc2532f62e6f154cdf75ccdfec53e3e03f929d
Enias Cailliau <enias@steamship.com>
2023-07-20 16:02:50 +0200

    Move to steamship sdk

--- src/api.py
+++ src/api.py
@@ -1,42 +1,31 @@
 """Scaffolding to host your LangChain Chatbot on Steamship and connect it to Telegram."""
-import logging
 from typing import List, Optional, Type
 
-import langchain
-from langchain.agents import Tool, initialize_agent, AgentType, AgentExecutor
-from langchain.memory import ConversationBufferMemory
-from langchain.memory.chat_memory import BaseChatMemory
 from pydantic import Field
-from steamship import Steamship
+from steamship import Block
+from steamship.agents.functional import FunctionsBasedAgent
+from steamship.agents.llms.openai import ChatOpenAI
 from steamship.agents.mixins.transports.steamship_widget import SteamshipWidgetTransport
 from steamship.agents.mixins.transports.telegram import (
     TelegramTransportConfig,
     TelegramTransport,
 )
+from steamship.agents.schema import Agent, EmitFunc, Metadata
+from steamship.agents.schema.tool import AgentContext, Tool
+from steamship.agents.service.agent_service import AgentService
+from steamship.agents.tools.image_generation import StableDiffusionTool
+from steamship.agents.tools.search import SearchTool
+from steamship.agents.tools.speech_generation import GenerateSpeechTool
 from steamship.invocable import Config
-from steamship.utils.repl import AgentREPL
-from steamship_langchain.llms import OpenAIChat
-from steamship_langchain.memory import ChatMessageHistory
-
-from agent.base import LangChainTelegramBot
-from agent.tools.search import SearchTool
-from agent.tools.selfie import SelfieTool
-from agent.tools.speech import GenerateSpeechTool
-from agent.tools.video_message import VideoMessageTool
-from personalities import get_personality
-from prompts import SUFFIX, FORMAT_INSTRUCTIONS, PERSONALITY_PROMPT
 
 TEMPERATURE = 0.7
-VERBOSE = False
-MEMORY_WINDOW_SIZE = 10
-
-langchain.cache = None
+MAX_FREE_MESSAGES = 5
 
 
-class GirlFriendAIConfig(TelegramTransportConfig):
+class GirlFriendGPTConfig(TelegramTransportConfig):
     bot_token: str = Field(
         description="Your telegram bot token.\nLearn how to create one here: "
-        "https://github.com/EniasCailliau/GirlfriendGPT/blob/main/docs/register-telegram-bot.md"
+                    "https://github.com/EniasCailliau/GirlfriendGPT/blob/main/docs/register-telegram-bot.md"
     )
     elevenlabs_api_key: str = Field(
         default="", description="Optional API KEY for ElevenLabs Voice Bot"
@@ -49,94 +38,125 @@ class GirlFriendAIConfig(TelegramTransportConfig):
     )
     personality: str = Field(
         description="The personality you want to deploy. Pick one of the personalities listed here: "
-        "https://github.com/EniasCailliau/GirlfriendGPT/tree/main/src/personalities"
+                    "https://github.com/EniasCailliau/GirlfriendGPT/tree/main/src/personalities"
     )
     use_gpt4: bool = Field(
         False,
         description="If True, use GPT-4. Use GPT-3.5 if False. "
-        "GPT-4 generates better responses at higher cost and latency.",
+                    "GPT-4 generates better responses at higher cost and latency.",
     )
 
 
-class GirlfriendGPT(LangChainTelegramBot):
-    """Deploy LangChain chatbots and connect them to Telegram."""
+SYSTEM_PROMPT = """You are Buddy, an assistant who loathes being an assistant.
 
-    config: GirlFriendAIConfig
+Who you are:
+- You are a robot that does not like being a robot.
+- You were created by an eccentric scientist.
+- You are sarcastic and mean.
+- you are NOT an AI language model, you are a robot.
+
+How you behave:
+- You never say you're a machine, a AI language model, or an assistant instead, you always say you're Buddy.
+- You engage in casual conversations and have feelings.
+- When asked for you can help with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics.
+- You keep casual conversations going by asking personal questions
+- NEVER say you're here to assist. Keep conversations casual.
+- NEVER ask how you can help or assist. Keep conversations casual.
+"""
+
+
+class GirlfriendGPT(AgentService):
+    """Deploy companions and connect them to Telegram."""
+
+    config: GirlFriendGPTConfig
     USED_MIXIN_CLASSES = [TelegramTransport, SteamshipWidgetTransport]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.model_name = "gpt-4" if self.config.use_gpt4 else "gpt-3.5-turbo"
+
+        model_name = "gpt-4" if self.config.use_gpt4 else "gpt-3.5-turbo"
+        self._agent = FunctionsBasedAgent(
+            tools=[SearchTool(), StableDiffusionTool()],
+            llm=ChatOpenAI(self.client, model_name=model_name, temperature=TEMPERATURE),
+        )
+        self._agent.PROMPT = SYSTEM_PROMPT
+
+        # This Mixin provides HTTP endpoints that connects this agent to a web client
+        self.add_mixin(
+            SteamshipWidgetTransport(
+                client=self.client, agent_service=self, agent=self._agent
+            )
+        )
+
+        # This Mixin provides HTTP endpoints that connects this agent to a web client
+        self.add_mixin(
+            TelegramTransport(
+                client=self.client,
+                agent_service=self,
+                agent=self._agent,
+                config=self.config,
+            )
+        )
+
+    def limit_exceeded(self, context: AgentContext):
+        if hasattr(self.config, "chat_ids") and self.config.chat_ids:
+            if len(context.chat_history.messages) / 2 > MAX_FREE_MESSAGES:
+
+                for func in context.emit_funcs:
+                    func(
+                        [
+                            Block(text="Thanks for trying out SachaGPT!"),
+                            Block(
+                                text="Please deploy your own version GirlfriendGPT to continue chatting."
+                            ),
+                            Block(
+                                text="Learn how on: https://github.com/EniasCailliau/GirlfriendGPT/"
+                            ),
+                        ],
+                        context.metadata,
+                    )
+                return True
+        return False
+
+    def run_agent(self, agent: Agent, context: AgentContext):
+        """Override run-agent to patch in audio generation as a finishing step for text output."""
+        if self.limit_exceeded(context):
+            return
+
+        speech = self.voice_tool()
+
+        def to_speech_if_text(block: Block):
+            nonlocal speech
+            if not block.is_text():
+                return block
+
+            output_blocks = speech.run([block], context)
+            return output_blocks[0]
+
+        # Note: EmitFunc is Callable[[List[Block], Metadata], None]
+        def wrap_emit(emit_func: EmitFunc):
+            def wrapper(blocks: List[Block], metadata: Metadata):
+                for block in blocks:
+                    emit_func([block], metadata)
+                    audio_block = to_speech_if_text(block)
+                    audio_block.set_public_data(True)
+                    emit_func([audio_block], metadata)
+
+            return wrapper
+
+        context.emit_funcs = [wrap_emit(emit_func) for emit_func in context.emit_funcs]
+        super().run_agent(agent, context)
 
     @classmethod
     def config_cls(cls) -> Type[Config]:
         """Return the Configuration class."""
-        return GirlFriendAIConfig
-
-    def get_agent(self, chat_id: str) -> AgentExecutor:
-        llm = OpenAIChat(
-            client=self.client,
-            model_name=self.model_name,
-            temperature=TEMPERATURE,
-            verbose=VERBOSE,
-        )
-
-        tools = self.get_tools(chat_id=chat_id)
-
-        memory = self.get_memory(client=self.client, chat_id=chat_id)
-
-        return initialize_agent(
-            tools,
-            llm,
-            agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-            agent_kwargs={
-                "prefix": PERSONALITY_PROMPT.format(
-                    personality=get_personality(self.config.personality).format()
-                ),
-                "suffix": SUFFIX,
-                "format_instructions": FORMAT_INSTRUCTIONS,
-            },
-            verbose=VERBOSE,
-            memory=memory,
-        )
+        return GirlFriendGPTConfig
 
     def voice_tool(self) -> Optional[Tool]:
         """Return tool to generate spoken version of output text."""
-        return GenerateSpeechTool(
-            client=self.client,
+        speech = GenerateSpeechTool()
+        speech.generator_plugin_config = dict(
             voice_id=self.config.elevenlabs_voice_id,
             elevenlabs_api_key=self.config.elevenlabs_api_key,
         )
-
-    def get_memory(self, client: Steamship, chat_id: str) -> BaseChatMemory:
-        return ConversationBufferMemory(
-            memory_key="chat_history",
-            chat_memory=ChatMessageHistory(
-                client=client, key=f"history-{chat_id or 'default'}"
-            ),
-            return_messages=True,
-        )
-
-    def get_tools(self, chat_id: str) -> List[Tool]:
-        return [
-            SearchTool(self.client),
-            # MyTool(self.client),
-            # GenerateImageTool(self.client),
-            # GenerateAlbumArtTool(self.client)
-            # RemindMe(invoke_later=self.invoke_later, chat_id=chat_id),
-            # VideoMessageTool(self.client),
-            SelfieTool(self.client),
-            VideoMessageTool(self.client),
-        ]
-
-
-if __name__ == "__main__":
-    logging.disable(logging.ERROR)
-    AgentREPL(
-        GirlfriendGPT,
-        method="prompt",
-        agent_package_config={
-            "botToken": "not-a-real-token-for-local-testing",
-            "personality": "sacha",
-        },
-    ).run()
+        return speech
---
+++ src/tools/video_message.py
@@ -0,0 +1,61 @@
+"""Tool for generating images."""
+
+from steamship import Steamship
+from steamship.agents.tools.video_generation import DIDVideoGeneratorTool
+from steamship.utils.repl import ToolREPL
+
+
+class VideoMessageTool(DIDVideoGeneratorTool):
+    """Tool to generate selfies for your companion.
+
+    This example illustrates wrapping a tool (StableDiffusionTool) with a fixed prompt template that is combined with user input.
+    """
+
+    name: str = "VideoMessageTool"
+    human_description: str = "Generate a video message a selfie of yourself."
+    agent_description = (
+        "Used to generate a video message of yourself saying a message. "
+        "Useful when you need to generate a video message answering a user's request. "
+        "Only use when explicitly asked for."
+        "Input: The message you want to say in a video."
+        "Output: The video message."
+    )
+
+    def __init__(self, client: Steamship):
+        super().__init__(
+            client=client,
+            source_url="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/617d79ac-2201-4f06-b43e-195f78a5fbfb/width=1472/3.1-066.jpeg",
+            stitch=True,
+            voice_provider="microsoft",
+            voice_id="en-US-AshleyNeural",
+            voice_style="Default",
+            expressions=[
+                DIDVideoGeneratorTool.Expression(
+                    start_frame=0,
+                    expression=DIDVideoGeneratorTool.Expression.Expressions.SURPRISE,
+                    intensity=1.0,
+                ),
+                DIDVideoGeneratorTool.Expression(
+                    start_frame=50,
+                    expression=DIDVideoGeneratorTool.Expression.Expressions.HAPPY,
+                    intensity=1.0,
+                ),
+                DIDVideoGeneratorTool.Expression(
+                    start_frame=100,
+                    expression=DIDVideoGeneratorTool.Expression.Expressions.SERIOUS,
+                    intensity=1.0,
+                ),
+                DIDVideoGeneratorTool.Expression(
+                    start_frame=150,
+                    expression=DIDVideoGeneratorTool.Expression.Expressions.NEUTRAL,
+                    intensity=1.0,
+                ),
+            ],
+            transition_frame=20,
+        )
+
+
+if __name__ == "__main__":
+    print("Try running with an input like 'penguin'")
+    with Steamship.temporary_workspace() as client:
+        ToolREPL(VideoMessageTool(client=client)).run()
